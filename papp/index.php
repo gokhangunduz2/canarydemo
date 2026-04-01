a9d88e5e33ccfaf8a38103e5e137822dcf017f06
Gökhan Gündüz <yazilimgg@gmail.com>
2026-04-01 12:03:51 +0000

    bug fixed

--- papp/index.php
+++ papp/index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<style>body {background-color:black; color:white;} </style>"
+echo "<style>body {background-color:black; color:white;} </style>";
 
 echo "VERSION: " . getenv("APP_VERSION");
