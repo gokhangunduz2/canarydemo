903bc3005ed059ac9ef9900f2a7de2b0c44140bd
Gökhan Gündüz <yazilimgg@gmail.com>
2026-04-01 13:23:24 +0000

    api fix

--- papp/index.php
+++ papp/index.php
@@ -8,7 +8,6 @@ echo "VERSION: " . getenv("APP_VERSION");
 
 
 <script>
-
 const lat = 41.00958937064345;
 const lng = 28.9776264472511;
 const params = 'airTemperature';
@@ -17,8 +16,29 @@ fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=$
   headers: {
     'Authorization': '993b2342-2dc8-11f1-beac-0242ac120004-993b2400-2dc8-11f1-beac-0242ac120004'
   }
-}).then((response) => response.json()).then((jsonData) => {
-  // Do something with response data.
-  console.log(jsonData)
+})
+.then((response) => response.json())
+.then((data) => {
+
+  const now = new Date();
+
+  let closest = null;
+  let minDiff = Infinity;
+
+  data.hours.forEach(item => {
+    const itemTime = new Date(item.time);
+
+    const diff = Math.abs(itemTime - now);
+
+    if (diff < minDiff) {
+      minDiff = diff;
+      closest = item;
+    }
+  });
+
+  const temp = closest.airTemperature.noaa;
+
+  console.log("Şu anki sıcaklık:", temp);
+
 });
 </script>
