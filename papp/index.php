e48213fc6d0fee226604d0ca3d02f9c1b44bac82
Gökhan Gündüz <yazilimgg@gmail.com>
2026-04-01 12:47:42 +0000

    API added

--- papp/index.php
+++ papp/index.php
@@ -2,3 +2,18 @@
 echo "<style>body {background-color:black; color:white;} </style>";
 
 echo "VERSION: " . getenv("APP_VERSION");
+
+?>
+
+const lat = 58.7984;
+const lng = 17.8081;
+const params = 'windSpeed';
+
+fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
+  headers: {
+    'Authorization': '993b2342-2dc8-11f1-beac-0242ac120004-993b2400-2dc8-11f1-beac-0242ac120004'
+  }
+}).then((response) => response.json()).then((jsonData) => {
+  // Do something with response data.
+  alert(response)
+});
